8438d25e34fe8bdad39d8f612f15772a3bc2a20d
asd412id <asdar.binsyam@gmail.com>
2023-12-12 15:35:07 +0800

    Add support for static path

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ const start = (() => {
     },
   });
 
+  const staticPath = path.join(__dirname, process.env.APP_STATIC_PATH || 'public');
+  fastify.register(require('@fastify/static'), {
+    root: staticPath,
+    prefix: '/',
+  });
+
+  fastify.setNotFoundHandler((request, reply) => {
+    const url = request.raw.url
+    if (url !== undefined && url.startsWith('/api/')) {
+      reply.status(404).send(errorResponse('API route not found', 404));
+    } else {
+      reply.sendFile('index.html')
+    }
+  })
+
   const port = process.env.APP_PORT || 8000;
   fastify.listen({ port }, () => {
     console.log(`Server start on port: ${port}`);
